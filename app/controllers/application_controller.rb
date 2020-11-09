97eab945a10bbcd3fc2479e5543569a54c2a4e26
Alice Fabre <alice.fabre@hotmail.fr>
2020-11-09 09:51:29 +0100

    update application controller with admin_controller? rules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :admin_controller?
+  before_action :authenticate_admin!, if: :admin_controller?
+
+  helper_method :admin_controller?
+
+  private
+
+  def admin_controller?
+    # !devise_controller? && request.path =~ /^\/admin/
+    params[:controller].split("/").first == "admin"
+  end
+
 end
