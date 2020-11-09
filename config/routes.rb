f5b96eb3c82daafd4bf6163f68a6156b745b7fd9
Alice Fabre <alice.fabre@hotmail.fr>
2020-11-09 09:48:06 +0100

    devise for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    root to: 'pages#home'
+    controller :pages do
+      get :home
+    end
+  end
 end
