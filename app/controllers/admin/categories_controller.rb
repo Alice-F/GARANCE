ea7a079f229664477af345ad13f8cc1197c353b0
Alice Fabre <alice.fabre@hotmail.fr>
2020-11-21 21:41:37 +0100

    add categories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,24 @@
 class Admin::CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+    authorize [:admin, @category]
+  end
+
+  def create
+    @category = Category.new(category_params)
+    authorize [:admin, @category]
+    if @category.save
+      redirect_to admin_home_path
+      flash[:notice] = "La nouvelle catégorie #{@category.name.downcase} est créée"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
