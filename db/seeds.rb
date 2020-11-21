278aa6b7b0012049c71b674254a12e581cfe9518
Alice Fabre <alice.fabre@hotmail.fr>
2020-11-21 13:40:20 +0100

    add admins to seed // db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,12 @@ puts 'Destroying'
 User.destroy_all
 Admin.destroy_all
 puts 'Destroying done'
+
+admin_attributes = [
+  { email: 'a@test.fr', password: 'plopplop' },
+  { email: 'b@test.fr', password: 'plopplop' },
+]
+
+puts 'Seeding Admins'
+Admin.create! admin_attributes
+puts 'Admins done'
