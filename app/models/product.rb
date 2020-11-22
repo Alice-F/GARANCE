364560ba9a0050d274dddf999a27cb928f8e1ed6
Alice Fabre <alice.fabre@hotmail.fr>
2020-11-22 14:14:12 +0100

    add validations and attachements to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :brand
   belongs_to :category
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :composition, presence: true
+  validates :brand, presence: true
+  validates :category, presence: true
+
+  has_one_attached :main_photo
+  has_many_attached :photos
 end
