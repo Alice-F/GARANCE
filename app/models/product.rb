b68f67365aa236190c711a89614977753e423095
Alice Fabre <alice.fabre@hotmail.fr>
2020-11-22 14:15:54 +0100

    add validations on photo and main_photo

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,8 @@ class Product < ApplicationRecord
   validates :composition, presence: true
   validates :brand, presence: true
   validates :category, presence: true
+  validates :main_photo, presence: true
+  validates :photos, length: { maximum: 4 }
 
   has_one_attached :main_photo
   has_many_attached :photos
