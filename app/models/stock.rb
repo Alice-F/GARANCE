71b22775be64e9476664a0e68e17150a7138265b
Alice Fabre <alice.fabre@hotmail.fr>
2020-11-26 23:05:48 +0100

    add has_one / through associations

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,8 @@
 class Stock < ApplicationRecord
   belongs_to :product
   belongs_to :size
+  has_one :brand, through: :product
+  has_one :category, through: :product
 
   validates :quantity, presence: true, numericality: { greater_than: 0 }
 end
